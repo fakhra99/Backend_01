4b92ff1acb2d27879b5b16b5b8695323d98c7b00
Malaika Saba <malaikasaba3@gmail.com>
2025-07-29 10:52:34 +0500

    Changes Added

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors"; 
+
 import { connectDB } from "./utils/connectToDb.js";
-import userRoutes from "./routes/userRoutes.js"
-import salaryRoutes from "./routes/salaryoute.js"
+import userRoutes from "./routes/userRoutes.js";
+import salaryRoutes from "./routes/salaryoute.js";
 
 dotenv.config();
 
@@ -11,11 +13,17 @@ const port = process.env.PORT || 5000;
 
 connectDB();
 
+// Use cors middleware (make sure this is BEFORE routes)
+app.use(cors({
+  origin: "http://localhost:5173", // Your Vite frontend port
+  credentials: true,
+}));
+
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
-app.use('/uploads', express.static('uploads'));
 app.use("/api/salaries", salaryRoutes);
+app.use("/uploads", express.static("uploads"));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
